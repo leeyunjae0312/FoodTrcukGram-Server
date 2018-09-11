173e857d2154e4e176f89438a9987ca482d87004
leeyunjea <yunjea0312@naver.com>
2018-09-11 19:41:27 +0900

    Add FoodTruckGramApplication

---
+++ src/main/java/kr/ac/hansung/FoodTruckGramApplication.java
@@ -0,0 +1,12 @@
+package kr.ac.hansung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodTruckGramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodTruckGramApplication.class, args);
+	}
+}
