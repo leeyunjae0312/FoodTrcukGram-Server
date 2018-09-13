d5c5fa43e610bef53b749c37c8c93df65ff0c2b0
leeyunjea <yunjea0312@naver.com>
2018-09-13 17:19:36 +0900

    Update getReview

--- src/main/java/kr/ac/hansung/dao/ReviewDAO.java
+++ src/main/java/kr/ac/hansung/dao/ReviewDAO.java
@@ -1,5 +1,7 @@
 package kr.ac.hansung.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 
@@ -7,8 +9,10 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import kr.ac.hansung.model.MenuInfo;
 import kr.ac.hansung.model.ReviewInfo;
 
 @Repository
@@ -22,8 +26,23 @@ public class ReviewDAO {
 	}
 
 	public List<ReviewInfo> getReview(Map<String, Object> param) {
-		// TODO Auto-generated method stub
-		return null;
+		String sqlStatement = "select * from review_tbl where storeName = ?";
+
+		return jdbcTemplate.query(sqlStatement, new Object[] { param.get("storeName") }, new RowMapper<ReviewInfo>() {
+
+			@Override
+			public ReviewInfo mapRow(ResultSet rs, int rowNum) throws SQLException {
+
+				ReviewInfo review = new ReviewInfo();
+
+				review.setUserName(rs.getString("userName"));
+				review.setReview(rs.getString("review"));
+				review.setDate(rs.getDate("date"));
+
+				return review;
+			}
+
+		});
 	}
 
 }
